9aee077f1e76a0e9d54d86f804e8d0ab0e82a396
kisssandoradam <kisssandoradam@gmail.com>
2015-03-26 17:31:23 +0100

    Fixed score calculator.

--- common/src/main/java/hu/smartcampus/appointmentscheduler/domain/solver/EventScheduleSimpleScoreCalculator.java
+++ common/src/main/java/hu/smartcampus/appointmentscheduler/domain/solver/EventScheduleSimpleScoreCalculator.java
@@ -8,7 +8,6 @@ import org.optaplanner.core.api.score.Score;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.impl.score.director.easy.EasyScoreCalculator;
 
-// TODO ezt erosen tuningolni kell ha megvannak a megfelelo parameterek.
 public class EventScheduleSimpleScoreCalculator implements EasyScoreCalculator<EventSchedule> {
 
 	@Override
@@ -18,13 +17,7 @@ public class EventScheduleSimpleScoreCalculator implements EasyScoreCalculator<E
 
 		for (Event outerEvent : solution.getEvents()) {
 			for (Event innerEvent : solution.getEvents()) {
-				if (outerEvent == innerEvent) {
-					continue;
-				}
-
-				// http://docs.jboss.org/optaplanner/release/6.2.0.CR4/optaplanner-docs/html_single/#immovablePlanningEntities
-				// TODO ez itt jo? nem kellene ez? outerEvent.isLocked() == true && innerEvent.isLocked() == true // Mi a kulonbseg?
-				if (outerEvent.isLocked() == innerEvent.isLocked()) {
+				if (outerEvent == innerEvent || outerEvent.isLocked() && innerEvent.isLocked()) {
 					continue;
 				}
 
