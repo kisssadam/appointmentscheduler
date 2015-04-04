8a0c63805b1478355ac4c50ddba0c9b2a8610db2
kisssandoradam <kisssandoradam@gmail.com>
2015-04-05 00:02:01 +0200

    Removed unnecessary lines.

--- appointment-scheduler-service/src/main/java/hu/smartcampus/appointmentscheduler/service/AppointmentSchedulerServiceImpl.java
+++ appointment-scheduler-service/src/main/java/hu/smartcampus/appointmentscheduler/service/AppointmentSchedulerServiceImpl.java
@@ -23,23 +23,23 @@ public class AppointmentSchedulerServiceImpl implements AppointmentSchedulerServ
 	private static final String SOLVER_CONFIG = "hu/smartcampus/appointmentscheduler/solver/eventScheduleSolverConfig.xml";
 
 	@Override
-	public Schedule schedule(String[] requiredLoginNames, String[] skippableLoginNames, DayOfWeek[] daysOfWeek, int year, int weekOfYear, int minHour, int maxHour) {
+	public Schedule schedule(String[] requiredLoginNames, String[] skippableLoginNames, DayOfWeek[] daysOfWeek,
+			int year, int weekOfYear, int minHour, int maxHour) {
 		UUID requestId = UUID.randomUUID();
 		logger.info("Creating new request: {}", requestId);
 		SolverFactory solverFactory = SolverFactory.createFromXmlResource(SOLVER_CONFIG);
 		Solver solver = solverFactory.buildSolver();
-		
+
 		logger.info("Start solving on request: {}", requestId);
 		EventScheduleFactory eventScheduleFactory = new EventScheduleFactory();
-		EventSchedule unsolvedEventSchedule = eventScheduleFactory.newEventSchedule(requiredLoginNames, skippableLoginNames, daysOfWeek, year, weekOfYear, minHour, maxHour);
-//		EventSchedule unsolvedEventSchedule = EventSchedule.createEventSchedule(requiredLoginNames, skippableLoginNames, daysOfWeek, year, weekOfYear, minHour, maxHour);
+		EventSchedule unsolvedEventSchedule = eventScheduleFactory.newEventSchedule(requiredLoginNames,
+				skippableLoginNames, daysOfWeek, year, weekOfYear, minHour, maxHour);
 		solver.solve(unsolvedEventSchedule);
-		
+
 		EventSchedule solvedEventSchedule = (EventSchedule) solver.getBestSolution();
 		logger.info("Request {} has been solved: {}", requestId, solvedEventSchedule);
-		
-		Schedule schedule = new Schedule(solvedEventSchedule);
-		return schedule;
+
+		return new Schedule(solvedEventSchedule);
 	}
 
 }
