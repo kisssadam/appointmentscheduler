a1f56348dbda54158c643237b34f3dc43baebb46
kisssandoradam <kisssandoradam@gmail.com>
2015-04-06 14:30:26 +0200

    Added class FormatUtilsTest.

---
+++ appointment-scheduler-common/src/test/java/hu/smartcampus/appointmentscheduler/utils/FormatUtilsTest.java
@@ -0,0 +1,50 @@
+package hu.smartcampus.appointmentscheduler.utils;
+
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+import java.time.DayOfWeek;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FormatUtilsTest {
+
+	private static Timestamp timestamp;
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		timestamp = Timestamp.valueOf("2015-04-01 14:20:30"); // Wednesday, week: 14.
+	}
+
+	@Test
+	public void testGetDayOfWeekFromTimeStamp() {
+		DayOfWeek day = FormatUtils.getDayOfWeekFromTimeStamp(timestamp);
+		assertEquals(DayOfWeek.WEDNESDAY, day);
+	}
+
+	@Test
+	public void testGetWeekFromTimestamp() {
+		int week = FormatUtils.getWeekFromTimestamp(timestamp);
+		assertEquals(14, week);
+	}
+	
+	@Test
+	public void testGetYearFromTimestamp() {
+		int year = FormatUtils.getYearFromTimestamp(timestamp);
+		assertEquals(2015, year);
+	}
+	
+	@Test
+	public void testGetHourFromTimestamp() {
+		int hour = FormatUtils.getHourFromTimestamp(timestamp);
+		assertEquals(14, hour);
+	}
+	
+	@Test
+	public void testMinuteFromTimestamp() {
+		int minute = FormatUtils.getMinuteFromTimestamp(timestamp);
+		assertEquals(20, minute);
+	}
+
+}
